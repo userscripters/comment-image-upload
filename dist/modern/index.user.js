922b963c31b9d9236545b766b19752f12e73b62a
double beep <38133098+double-beep@users.noreply.github.com>
2024-10-08 10:55:41 +0000

    0.2.0

--- dist/modern/index.user.js
+++ dist/modern/index.user.js
@@ -1,44 +1,45 @@
 // ==UserScript==
-// @name           Comment Image Upload
-// @author         Oleg Valter <oleg.a.valter@gmail.com>
-// @description    Userscript for uploading comment images
-// @grant          none
-// @homepage       https://github.com/userscripters/comment-image-upload#readme
-// @license        GPL-3.0-or-later
-// @match          https://stackoverflow.com/questions/*
-// @match          https://serverfault.com/questions/*
-// @match          https://superuser.com/questions/*
-// @match          https://*.stackexchange.com/questions/*
-// @match          https://meta.superuser.com/questions/*
-// @match          https://meta.serverfault.com/questions/*
-// @match          https://askubuntu.com/questions/*
-// @match          https://meta.askubuntu.com/questions/*
-// @match          https://stackapps.com/questions/*
-// @match          https://mathoverflow.net/questions/*
-// @match          https://meta.mathoverflow.net/questions/*
-// @match          https://pt.stackoverflow.com/questions/*
-// @match          https://pt.meta.stackoverflow.com/questions/*
-// @match          https://meta.stackoverflow.com/questions/*
-// @match          https://ja.stackoverflow.com/questions/*
-// @match          https://ja.meta.stackoverflow.com/questions/*
-// @match          https://ru.stackoverflow.com/questions/*
-// @match          https://ru.meta.stackoverflow.com/questions/*
-// @match          https://es.stackoverflow.com/questions/*
-// @match          https://es.meta.stackoverflow.com/questions/*
-// @match          https://meta.meta.superuser.com/questions/*
-// @match          https://meta.meta.serverfault.com/questions/*
-// @match          https://meta.meta.askubuntu.com/questions/*
-// @match          https://meta.meta.mathoverflow.net/questions/*
-// @match          https://pt.meta.meta.stackoverflow.com/questions/*
-// @match          https://meta.meta.stackoverflow.com/questions/*
-// @match          https://ja.meta.meta.stackoverflow.com/questions/*
-// @match          https://ru.meta.meta.stackoverflow.com/questions/*
-// @match          https://es.meta.meta.stackoverflow.com/questions/*
-// @namespace      userscripters
-// @run-at         document-body
-// @source         git+https://github.com/userscripters/comment-image-upload.git
-// @supportURL     https://github.com/userscripters/comment-image-upload/issues
-// @version        0.1.0
+// @name            Comment Image Upload
+// @author          Oleg Valter <oleg.a.valter@gmail.com>
+// @contributors    double beep (https://github.com/double-beep)
+// @description     Userscript for uploading comment images
+// @grant           none
+// @homepage        https://github.com/userscripters/comment-image-upload#readme
+// @license         GPL-3.0-or-later
+// @match           https://stackoverflow.com/questions/*
+// @match           https://serverfault.com/questions/*
+// @match           https://superuser.com/questions/*
+// @match           https://*.stackexchange.com/questions/*
+// @match           https://meta.superuser.com/questions/*
+// @match           https://meta.serverfault.com/questions/*
+// @match           https://askubuntu.com/questions/*
+// @match           https://meta.askubuntu.com/questions/*
+// @match           https://stackapps.com/questions/*
+// @match           https://mathoverflow.net/questions/*
+// @match           https://meta.mathoverflow.net/questions/*
+// @match           https://pt.stackoverflow.com/questions/*
+// @match           https://pt.meta.stackoverflow.com/questions/*
+// @match           https://meta.stackoverflow.com/questions/*
+// @match           https://ja.stackoverflow.com/questions/*
+// @match           https://ja.meta.stackoverflow.com/questions/*
+// @match           https://ru.stackoverflow.com/questions/*
+// @match           https://ru.meta.stackoverflow.com/questions/*
+// @match           https://es.stackoverflow.com/questions/*
+// @match           https://es.meta.stackoverflow.com/questions/*
+// @match           https://meta.meta.superuser.com/questions/*
+// @match           https://meta.meta.serverfault.com/questions/*
+// @match           https://meta.meta.askubuntu.com/questions/*
+// @match           https://meta.meta.mathoverflow.net/questions/*
+// @match           https://pt.meta.meta.stackoverflow.com/questions/*
+// @match           https://meta.meta.stackoverflow.com/questions/*
+// @match           https://ja.meta.meta.stackoverflow.com/questions/*
+// @match           https://ru.meta.meta.stackoverflow.com/questions/*
+// @match           https://es.meta.meta.stackoverflow.com/questions/*
+// @namespace       userscripters
+// @run-at          document-body
+// @source          git+https://github.com/userscripters/comment-image-upload.git
+// @supportURL      https://github.com/userscripters/comment-image-upload/issues
+// @version         0.2.0
 // ==/UserScript==
 
 "use strict";
